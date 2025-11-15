d30108deb6549157f9d40cb46d5082fdf6c6ab8d
Binard2112 <mait58674@gmail.com>
2025-11-15 09:37:37 +0700

    feat(Q1): init expo project and sqlite setup for habit tracker

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,17 @@
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
+import { SQLiteProvider } from "expo-sqlite";
+import { initTable } from "@/db/db";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <SQLiteProvider databaseName="app.db" onInit={(db) => initTable(db)}>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <Stack screenOptions={{ headerShown: false }}></Stack>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </SQLiteProvider>
+  );
 }
---
+++ src/types/habit.ts
@@ -0,0 +1,8 @@
+export type Habit = {
+  id: number;
+  title: string;
+  description: string;
+  active: Boolean;
+  done_today: Boolean;
+  created_at: number;
+};
